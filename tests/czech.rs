ae36d04074d746641245bae63e14cd1384ce67b9
Thomas ten Cate <ttencate@gmail.com>
2019-01-12 12:42:38 +0100

    Make failing test for Zork 1 to point out the input processing issue

--- tests/czech.rs
+++ tests/czech.rs
@@ -1,6 +1,5 @@
 mod platform;
 
-use difference::Changeset;
 use crate::platform::TestPlatform;
 use rustyknife::*;
 use std::fs;
@@ -12,14 +11,10 @@ fn test_czech() {
     // platform.enable_trace();
     println!("{}", Memory::wrap(data.clone().into()).unwrap().obj_table().to_tree_string().unwrap());
     let mut z = ZMachine::new(&mut platform, data).unwrap();
+
     if let Err(err) = z.run() {
         panic!("{}\nOutput:\n{}", err, platform.take_output());
     }
-
-    let actual_output = platform.take_output();
     let expected_output = String::from_utf8(fs::read("tests/czech/czech.out3").unwrap()).unwrap();
-    let diff = Changeset::new(&actual_output, &expected_output, "");
-    assert_eq!(actual_output, expected_output);
-    assert!(actual_output == expected_output,
-        "CZECH output did not match expected output. Difference (green = expected, red = actual):\n\n{}\n", diff);
+    platform.expect_output(&expected_output);
 }
--- tests/zork1.rs
+++ tests/zork1.rs
@@ -8,9 +8,12 @@ use std::fs;
 fn test_zork1() {
     let mut platform = TestPlatform::new();
     let data = fs::read("tests/zork1/zork1.z3").unwrap();
-    let mut _z = ZMachine::new(&mut platform, data).unwrap();
-    // TODO check some input and output here
-    // loop {
-    //     z.step().unwrap();
-    // }
+
+    platform.add_input("look");
+    platform.add_input("quit");
+    let mut z = ZMachine::new(&mut platform, data).unwrap();
+    if let Err(err) = z.run() {
+        panic!("{}\nOutput:\n{}", err, platform.take_output());
+    }
+    platform.expect_output(&"");
 }
